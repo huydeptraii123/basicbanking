f7a6d83f574c2467eb25a97d4ec9550d5afe3736
huydeptraii123 <kristopherhuy@gmail.com>
2025-09-20 04:06:41 +0700

    Implement plaid and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -26,6 +26,7 @@ import { Loader2 } from 'lucide-react'
 import SignUp from '@/app/(auth)/sign-up/page'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -55,12 +56,27 @@ const AuthForm = ({ type }: {type: string}) => {
     setIsLoading(true)
     try {
         // Simulate an API call
+        
 
         if(type === 'sign-up') {
-            const newUser = await signUp(data);
+            const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            ssn: data.ssn!,
+            email: data.email,
+            password: data.password
+            };
+          
+            const newUser = await signUp(userData);
 
             setUser(newUser);
         }
+      
 
         if(type === 'sign-in') {
             const response = await signIn({
@@ -110,7 +126,7 @@ const AuthForm = ({ type }: {type: string}) => {
         </header>
       {user ? (
         <div className="flex flex-col gap-4">
-            {/* {PlaidLink} */}
+            <PlaidLink user={user} variant="primary" />
         </div>
       ) : (
         <>
